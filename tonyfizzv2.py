0b08188a8f73b622a99ef5df3f2948d0302564c8
thughes42 <49875669+thughes42@users.noreply.github.com>
2019-05-02 11:06:50 -0500

    Add files via upload

---
+++ tonyfizzv2.py
@@ -0,0 +1,14 @@
+# Siz FizzBuzzington the 3rd #
+
+         ##   _--_     ##
+        ####-(╭ರ_•́) - ####
+
+for num in range(1,21):
+    if num % 15 is 0:
+        print('FizzBuzz')
+    elif num % 3 is 0:
+        print('Fizz')
+    elif num % 5 is 0:
+        print('Buzz')
+    else:
+        print(num)
